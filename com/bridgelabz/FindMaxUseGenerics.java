88d2eced40d581c57f57598ffda16a9aa808be1f
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-12-06 23:12:13 +0530

    Extend MaxNumber method Print the max using generic method

--- com/bridgelabz/FindMaxUseGenerics.java
+++ com/bridgelabz/FindMaxUseGenerics.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 /**
  * @author Govard hanReddy
  */
-public class FindMaxUseGenerics<T> {
+public class FindMaxUseGenerics {
     /**
-     * find the maximum values using generic class
+     * find the maximum values using generic method
      */
 
-    public void maxNumber(T[] array) {
+    public static<T> void maxNumber(T[] array) {
         /**
-         * maxNumber Generic class used to find max value and return max value
+         * maxNumber Generic method used to find max value and return max value
          * use Arrays class sort static method to print the array in ascending order
          */
         Arrays.sort(array);
@@ -20,21 +20,23 @@ public class FindMaxUseGenerics<T> {
             System.out.print(array[i] + " ");
         }
         System.out.println();
+        System.out.println("Maximum value is "+array[array.length-1]);
     }
 
     public static void main(String[] args) {
         /**
-         * In this method use class level generics to print all object array in single generic
+         * In this method use method level generics to print all object array in single generic method
          */
-        FindMaxUseGenerics max = new FindMaxUseGenerics();
         Integer[] integerArray = {1, 5, 7, 8, 9, 6, 4};
         String[] stringArray = {"Ram", "Ravi", "Raju", "Surya"};
         Float[] floatArray = {1.5f, 6.5f, 4.5f, 8.4f, 1.3f, 6.5f};
         System.out.println("Print Integer array :- ");
-        max.maxNumber(integerArray);
+        maxNumber(integerArray);
+        System.out.println("************************");
         System.out.println("Print Float array :- ");
-        max.maxNumber(floatArray);
+        maxNumber(floatArray);
+        System.out.println("************************");
         System.out.println("Print String array :- ");
-        max.maxNumber(stringArray);
+        maxNumber(stringArray);
     }
 }
